3cac2c7c498437ddc322ca1b8bb8f7b8e03102b7
Piyush Mourya <47456936+mauryapiyush76@users.noreply.github.com>
2021-11-03 10:15:48 +0530

    Add files via upload

---
+++ kanban-board-application-backend-main/src/test/java/io/project/kba/KanbanBoardApplicationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.project.kba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanBoardApplicationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
